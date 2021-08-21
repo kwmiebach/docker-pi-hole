58aeb7ac78b70f1d586d668839fe2e69c6c2b576
kwmiebach <kwmiebach@users.noreply.github.com>
2021-08-21 15:47:06 +0200

    docker_run.sh for n29

--- docker_run.sh
+++ docker_run.sh
@@ -2,26 +2,34 @@
 
 # https://github.com/pi-hole/docker-pi-hole/blob/master/README.md
 
+HOSTNAME=n29
+SERVER_IP=192.168.10.29
+PIHOLE_BASE=/home/rock/pihole
+
 PIHOLE_BASE="${PIHOLE_BASE:-$(pwd)}"
 [[ -d "$PIHOLE_BASE" ]] || mkdir -p "$PIHOLE_BASE" || { echo "Couldn't create storage directory: $PIHOLE_BASE"; exit 1; }
+
+printf 'Starting up pihole container ... '
 
 # Note: ServerIP should be replaced with your external ip.
 docker run -d \
     --name pihole \
     -p 53:53/tcp -p 53:53/udp \
     -p 80:80 \
-    -e TZ="America/Chicago" \
+    -e TZ="Europe/Berlin" \
     -v "${PIHOLE_BASE}/etc-pihole/:/etc/pihole/" \
     -v "${PIHOLE_BASE}/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
-    --dns=127.0.0.1 --dns=1.1.1.1 \
+    --dns=127.0.0.1 --dns=8.8.8.8 \
     --restart=unless-stopped \
-    --hostname pi.hole \
-    -e VIRTUAL_HOST="pi.hole" \
-    -e PROXY_LOCATION="pi.hole" \
-    -e ServerIP="127.0.0.1" \
+    --hostname $HOSTNAME \
+    -e VIRTUAL_HOST="$HOSTNAME" \
+    -e PROXY_LOCATION="$HOSTNAME" \
+    -e ServerIP="$SERVER_IP" \
     pihole/pihole:latest
 
-printf 'Starting up pihole container '
+printf '\nChecking container ...'
+
+
 for i in $(seq 1 20); do
     if [ "$(docker inspect -f "{{.State.Health.Status}}" pihole)" == "healthy" ] ; then
         printf ' OK'
